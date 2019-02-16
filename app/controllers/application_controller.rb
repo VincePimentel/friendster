7a6087b7fbb623cb3035ceb14d34e9075e81bdd6
VincePimentel <contact@vincep.me>
2019-02-16 12:04:26 -0800

    Add login and logout features via sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,7 @@ require 'pry'
 
 class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :current_user
-
-  def logged_in?
-    !!session[:user_id]
-  end
+  include SessionsHelper
 
   def redirect_if_logged_out
     redirect_to new_session_path if !logged_in?
