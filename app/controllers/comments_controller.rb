076a4fbb0c096ee914f79e9eb1b4df0cf11a6fe8
VincePimentel <contact@vincep.me>
2019-02-17 12:25:43 -0800

    Add accordion to display post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,22 @@
 class CommentsController < ApplicationController
 
   def create
+    post = Post.find(params[:post_id])
 
+    @comment = post.comments.build(comment_params)
 
+    @comment.save
+
+    redirect_to root_path
   end
 
   def destroy
 
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
