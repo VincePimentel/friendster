39aa5bf8a6d744ab2f74d9722ddc064a70420983
VincePimentel <contact@vincep.me>
2019-02-21 18:29:20 -0800

    Revert "Revert to b2edc99 due to friends breaking"
    This reverts commit eb10b0fc595229b88f3927619e4accf5a9c7acbf.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,22 +1,47 @@
 module PostsHelper
 
   def post_header(post)
-    <% if post.recipient_id == post.user.id %>
-            <%= link_to post.user.full_name, user_path(post.user) %>
-          <% else %>
-            <%= link_to post.user.full_name, user_path(post.user) %> <%= icon("fas", "caret-right") %> <%= link_to User.find(post.recipient_id).full_name, user_path(post.recipient_id) %> <!-- helperize me -->
-          <% end %>
+    post_header_info(post) + post_header_controls(post)
+  end
 
-          <% if current_user == post.user %>
-            <span class="float-right">
-              <%= link_to icon("fas", "edit"),
-                edit_user_post_path(post.user.id, post) %>
-              <%= link_to icon("fas", "trash"),
-                user_post_path(post.user.id, post),
-                method: :delete,
-                data: { confirm: "Are you sure?" } %>
-            </span>
-          <% end %>
+  def post_header_info(post)
+    if post.recipient_id == post.user.id
+      link_to(post.user.full_name, user_path(post.user))
+    else
+      link_to(post.user.full_name, user_path(post.user)) + " " + icon("fas", "caret-right") + " " + link_to(User.find(post.recipient_id).full_name, user_path(post.recipient_id))
+    end
+  end
+
+  def post_header_controls(post)
+    if current_user == post.user
+      tag.span class: "float-right" do
+        link_to(icon("fas", "edit"),
+          edit_user_post_path(post.user.id, post)) + " " + link_to(icon("fas", "trash"),
+          user_post_path(post.user.id, post),
+          method: :delete,
+          data: { confirm: "Are you sure?" })
+      end
+    end
+  end
+
+  def post_body(post)
+    if post.content.empty?
+      post_body_image(post)
+    else
+      post_body_content(post) + post_body_image(post)
+    end
+  end
+
+  def post_body_content(post)
+    tag.div post.content, class: "col py-3"
+  end
+
+  def post_body_image(post)
+    if !post.link.empty?
+      link_to post.link, target: "_blank" do
+        image_tag post.link, class: "img-fluid"
+      end
+    end
   end
 
   def post_datetime(post)
